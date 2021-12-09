53a7bdf8709a3b51bb9a7663ecaa5a71ff9690dd
bernardobdr <bernardosilvabdr@gmail.com>
2021-12-09 17:57:59 -0300

    adicionando eventos

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var escolha = document.querySelector('escolhaComoDecifrar')
-var formCesar = document.querySelector("formCesar")
-var formBase = document.querySelector("formBase64")
+var escolha = document.querySelector('#escolhaComoDecifrar')
+var formCesar = document.querySelector('#formCifraCesar')
+var formBase = document.querySelector('#formBase64')
 
-metodo.addEventListener("change", function () {
+metodo.addEventListener('change', function () {
   if (metodo.value == 0) {
-    formCesar.style.display = "none"
+    formCesar.style.display = 'none'
     formBase.style.display = "none"
   } else if (metodo.value == 1) {
     formCesar.style.display = "flex"
@@ -19,3 +19,15 @@ var radioCifraDeCesar = document.querySelectorAll(".radio")
 var radioBase64 = document.querySelectorAll(".radio2")
 var btnCodDecod = document.getElementById("codDec")
 var btnCodDecod2 = document.getElementById("codDec2")
+
+radioCesar[0].addEventListener('click', function () {
+  if (radioCesar[0].checked) {
+    btnCodDecod.innerHTML = 'Mensagem codificada:';
+  }
+});
+
+radioCesar[1].addEventListener('click', function () {
+  if (radioCesar[1].checked) {
+    btnCodDecod.innerHTML = 'Mensagem decodificado:'
+  }
+})
