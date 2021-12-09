f35f295db057f0b39824caa204a140e6635ae735
bernardobdr <bernardosilvabdr@gmail.com>
2021-12-09 17:01:13 -0300

    continuando no javascript no formulário de cesar

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 var metodo = document.getElementById('escolhaComoDecifrar')
 var formCesar = document.getElementById("formCesar");
 var formBase = document.getElementById("formBase");
+
+metodo.addEventListener("change", function () {
+  if (metodo.value == 0) {
+    formCesar.style.display = "none";
+    formBase.style.display = "none";
+  } else if (metodo.value == 1) {
+    formCesar.style.display = "flex";
+    formBase.style.display = "none";
+  } else if (metodo.value == 2) {
+    formBase.style.display = "flex";
+    formCesar.style.display = "none";
+  }
+});
