cbb964d42cad1d2797978d856c517f68380df977
bernardobdr <bernardosilvabdr@gmail.com>
2021-12-09 16:54:24 -0300

    continuando alteração no formulário e javascript

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-var selecao = document.querySelector('#selecao')
-var telaDeCesar = document.querySelector('#divCifraDeCesar')
-var telaDaBase64 = document.querySelector('#divBase64')
-
-selecao.addEventListener('change', function(){
-  if(selecao.value == 'base64'){
-    telaDaBase64.style.display = 'block'
-    telaDeCesar.style.display = 'none'
-  }else{
-    telaDeCesar.style.display = 'block'
-    telaDaBase64.style.display = 'none'
-  }
-})
+var metodo = document.getElementById('escolhaComoDecifrar')
+var formCesar = document.getElementById("formCesar");
+var formBase = document.getElementById("formBase");
