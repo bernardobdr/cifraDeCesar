8791fe48e82a5eda3323e9ba8b6a94a169686efa
bernardobdr <bernardosilvabdr@gmail.com>
2021-12-09 13:56:17 -0300

    continuando a estilização junto com o javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+var selecao = document.querySelector('#selecao')
+var telaDeCesar = document.querySelector('#divCesar')
+var telaDeBase64 = document.querySelector('#divBase64')
+
+selecao.addEventListener('change', function(){
+  if(selecao.value == base64){
+    telaDaBase64.style.display = 'block'
+    telaDeCesar.style.display = 'none'
+  }else{
+    telaDaBase64.style.display = 'none'
+    telaDeCesar.style.display = 'blick'
+  }
+})
