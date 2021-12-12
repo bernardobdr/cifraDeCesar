e1e0030a7b59e783639a3ba5715fbfd3e1a05e3b
bernardobdr <bernardosilvabdr@gmail.com>
2021-12-12 15:25:30 -0300

    efetuando alterações no projeto cifra de cesar

--- script.js
+++ script.js
@@ -1,102 +1,131 @@
-var escolha = document.querySelector('#escolhaComoDecifrar')
-var formCesar = document.querySelector('#formCifraCesar')
-var formBase64 = document.querySelector('#formBase64')
+var escolha = document.querySelector('escolha');
+var formCesar = document.querySelector("formCesar");
+var formBase = document.querySelector("formBase");
 
-escolha.addEventListener('change', function () {
-  if (escolha.value == 0) {
-    formCesar.style.display = 'none'
-    formBase64.style.display = 'none'
-  } else if (escolha.value == 1) {
-    formCesar.style.display = 'flex'
-    formBase64.style.display = 'none'
-  } else if (escolha.value == 2) {
-    formBase64.style.display = 'flex'
-    formCesar.style.display = 'none'
+metodo.addEventListener("change", function () {
+  if (metodo.value == 0) {
+    formCesar.style.display = "none";
+    formBase.style.display = "none";
+  } else if (metodo.value == 1) {
+    formCesar.style.display = "flex";
+    formBase.style.display = "none";
+  } else if (metodo.value == 2) {
+    formBase.style.display = "flex";
+    formCesar.style.display = "none";
   }
 });
+
 // Mudar botão de decodificar e codificar
 
-var radioCifraDeCesar = document.querySelectorAll('.radio')
-var radioBase64 = document.querySelectorAll('.radio-2')
-var btnCodificarDecodificar1 = document.getElementById('codDec')
-var btnCodificarDecodificar2 = document.getElementById("codDec2")
+var radioCesar = document.querySelectorAll(".radio");
+var radioBase = document.querySelectorAll(".radio2");
+var btnCodDecod = document.getElementById("codDec");
+var btnCodDecod2 = document.getElementById("codDec2");
 
-radioCesar[0].addEventListener('click', function () {
+radioCesar[0].addEventListener("click", function () {
   if (radioCesar[0].checked) {
-    btnCodDecod.innerHTML = 'Mensagem codificada:'
+    btnCodDecod.innerHTML = "Resultado codificado:";
   }
 });
 
-radioCesar[1].addEventListener('click', function () {
+radioCesar[1].addEventListener("click", function () {
   if (radioCesar[1].checked) {
-    btnCodDecod.innerHTML = 'Mensagem decodificado:'
+    btnCodDecod.innerHTML = "Resultado decodificado:";
   }
-})
+});
 
-radioBase[0].addEventListener('click', function () {
+radioBase[0].addEventListener("click", function () {
   if (radioBase[0].checked) {
-    btnCodDecod2.innerHTML = 'Resultado codificado:'
+    btnCodDecod2.innerHTML = "Resultado codificado:";
   }
-})
+});
 
-radioBase[1].addEventListener('click', function () {
+radioBase[1].addEventListener("click", function () {
   if (radioBase[1].checked) {
-    btnCodDecod2.innerHTML = 'Mensagem decodificado:'
+    btnCodDecod2.innerHTML = "Resultado decodificado:";
   }
-})
+});
 
-var entrada = document.getElementById('entrada')
-var saida = document.getElementById('saida')
-var chavePasso = document.getElementById('chavePasso')
-var valueChave
+// Criptografia Cifra de César
 
-entrada.addEventListener('keyup', function () {
+var entrada = document.getElementById("entrada");
+var saida = document.getElementById("saida");
+var chavePasso = document.getElementById("chavePasso");
+var valueChave;
+
+entrada.addEventListener("keyup", function () {
   if (radioCesar[0].checked) {
-    var valueText = entrada.value.split('')
-    valueChave = parseInt(chavePasso.value)
-    saida.value = codifica(valueText, valueChave)
+    var valueText = entrada.value.split("");
+    valueChave = parseInt(chavePasso.value);
+    saida.value = codifica(valueText, valueChave);
   } else {
-    var valueText = entrada.value.split('')
-    valueChave = parseInt(chavePasso.value)
+    var valueText = entrada.value.split("");
+    valueChave = parseInt(chavePasso.value);
     saida.value = decodifica(valueText, valueChave);
   }
-})
-
+});
 
 function codifica(texto, chavePasso) {
   return (
     texto
+      // map é um for que transforma string em array automaticamente
       .map((str) => {
-        var cripto = str.charCodeAt()
+        // famosa arrow function
+        var cripto = str.charCodeAt();
+        // limitar o ascii a letras do alfabeto maiusculas e minusculas
         if (cripto >= 65 && cripto <= 90) {
-          return String.fromCharCode(((cripto - 65 + chavePasso) % 26) + 65)
+          // retornar em string
+          return String.fromCharCode(((cripto - 65 + chavePasso) % 26) + 65);
         } else if (cripto >= 97 && cripto <= 122) {
-          return String.fromCharCode(((cripto - 97 + chavePasso) % 26) + 97)
+          return String.fromCharCode(((cripto - 97 + chavePasso) % 26) + 97);
         } else {
-          return str
+          return str;
         }
       })
-      .join('')
-  )
+      // juntar tudo em uma string novamente
+      .join("")
+  );
 }
 
 function decodifica(texto, chavePasso) {
   return (
     texto
+      // map é um for que transforma string em array automaticamente
       .map((str) => {
-        var cripto = str.charCodeAt()
+        // famosa arrow function
+        var cripto = str.charCodeAt();
+        // limitar o ascii a letras do alfabeto maiusculas e minusculas
         if (cripto >= 65 && cripto <= 90) {
-          return String.fromCharCode(cripto - 65 - chavePasso < 0)?
+          // retornar em string
+          return String.fromCharCode(cripto - 65 - chavePasso < 0)
+            ? // if e else como operador condicional ternario
               String.fromCharCode(((cripto - 65 - chavePasso + 26) % 26) + 65)
-            : String.fromCharCode(((cripto - 65 - chavePasso) % 26) + 65)
+            : String.fromCharCode(((cripto - 65 - chavePasso) % 26) + 65);
         } else if (cripto >= 97 && cripto <= 122) {
           return String.fromCharCode(cripto - 97 - chavePasso < 0)
             ? String.fromCharCode(((cripto - 97 - chavePasso + 26) % 26) + 97)
-            : String.fromCharCode(((cripto - 97 - chavePasso) % 26) + 65)
+            : String.fromCharCode(((cripto - 97 - chavePasso) % 26) + 65);
         } else {
           return str;
         }
       })
+      // juntar tudo em uma string novamente
       .join("")
-  )
+  );
 }
+
+// Criptografia do Base64
+var entradaBase = document.getElementById("entrada2");
+var saida2 = document.getElementById("saida2");
+var baseValue;
+
+entradaBase.addEventListener("keyup", function () {
+  baseValue = entradaBase.value;
+  if (radioBase[0].checked) {
+    saida2.value = btoa(baseValue);
+  } else if (radioBase[1].checked) {
+    saida2.value = atob(baseValue);
+  } else {
+    return 0;
+  }
+});
